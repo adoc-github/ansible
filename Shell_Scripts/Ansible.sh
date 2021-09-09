f323ed230247ba2e13e2db084d1b047fe3977942
adoc-github <65012009+adoc-github@users.noreply.github.com>
2021-09-09 13:45:49 +0900

    Update Ansible.sh

--- Shell_Scripts/Ansible.sh
+++ Shell_Scripts/Ansible.sh
@@ -15,19 +15,12 @@ yum update -y
 
 
 echo "****************************************************************"
-echo "Installing Ansible"
+echo "Installing Repository"
 echo "****************************************************************"
-#yum install ansible -y
-
-
-echo "****************************************************************"
-echo "Installing Ansible Modules"
-echo "****************************************************************"
-#yum install php7.0 php7.0-mysql libapache2-mod-php7.0 php7.0-cli php7.0-cgi php7.0-gd -y
-
+yum install epel-release -y
 
 
 echo "****************************************************************"
 echo "Installing Ansible"
 echo "****************************************************************"
-#yum install ansible -y
+yum install ansible -y
